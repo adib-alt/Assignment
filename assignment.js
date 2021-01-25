bee00cf1d91537ce9bff5da19a5bbdfcdd9daade
Adib-alt <farjanasume22@gmail.com>
2021-01-25 14:22:32 +0600

    Assignment

---
+++ assignment.js
@@ -0,0 +1,39 @@
+//kilometerToMeter
+
+function kilometerToMeter(x){
+
+var result = x*1000;
+return result;
+
+}
+
+//budgetCalculator
+
+function budgetCalculator(watch, phone, laptop){
+
+var budget = watch*50 + phone*100 + laptop*500;
+
+return budget;
+
+}
+
+//hotelCost
+
+function hotelCost(days){
+if(days <= 10){
+    var result = days*100;
+    return result; 
+}
+
+else if(days > 10){
+    var minus = days-10;
+    var result = 10*100 + minus*80;
+    return result;
+}
+}
+
+//megaFriend
+
+
+
+
